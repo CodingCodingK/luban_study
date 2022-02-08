5dd7ee0c4612ca62479dd9ed25ce98458f3bf478
CodingCodingK <2470939431@qq.com>
2022-02-08 18:41:25 +0800

    0208

---
+++ luban_study/Assets/Scripts/Test.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using cfg;
+using cfg.item;
+using SimpleJSON;
+using System.IO;
+
+public class Test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Tables table = new Tables(Loader);
+        Item item = table.TbItem.Get(10010);
+        Debug.Log($"{item.Name}    {item.Desc}");
+    }
+
+    private JSONNode Loader(string fileName)
+    {
+        return JSON.Parse(File.ReadAllText(Application.dataPath + "/../GenerateDatas/json/" + fileName + ".json"));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
