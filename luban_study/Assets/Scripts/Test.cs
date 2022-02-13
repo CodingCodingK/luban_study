8afd0b322ae0d269bd9aa44104d219891c728153
CodingCodingK <2470939431@qq.com>
2022-02-13 17:44:56 +0800

    0213

--- luban_study/Assets/Scripts/Test.cs
+++ luban_study/Assets/Scripts/Test.cs
@@ -1,30 +1,75 @@
-﻿// using System.Collections;
-// using System.Collections.Generic;
-// using UnityEngine;
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
 // using cfg;
+using SimpleJSON;
+using System.IO;
+using Bright.Serialization;
+using proto;
+using proto.test;
+
 // using cfg.Datas;
-// using cfg.Constants;
-// using SimpleJSON;
-// using System.IO;
-//
-// public class Test : MonoBehaviour
-// {
-//     // Start is called before the first frame update
-//     void Start()
-//     {
-//         Tables table = new Tables(JsonLoader);
-//         Equip equip = table.Weapon.Get(1);
-//         Debug.Log($"{equip.Name}    {equip.Color}    {equip.Quality}");
-//     }
-//
-//     private JSONNode JsonLoader(string fileName)
-//     {
-//         return JSON.Parse(File.ReadAllText(Application.dataPath + "/../GenerateDatas/json/" + fileName + ".json"));
-//     }
-//
-//     // Update is called once per frame
-//     void Update()
-//     {
-//         
-//     }
-// }
+// using cfg.Equips;
+
+public class Test : MonoBehaviour
+{
+    void Start()
+    {
+        
+        // Tables table = new Tables(JsonLoader);
+        // Weapon equip = table.TbWeapon.Get(1);
+        // Debug.Log($"{equip.Name}    {equip.Color}    {equip.SPD}");
+        // foreach (var skill in equip.Skill_Ref)
+        // {
+        //     Debug.Log($"{skill.SkillId}    {skill.SkillName}    {skill.SkillTime}");
+        // }
+
+        NetMsg msg = new NetMsg()
+        {
+            Cmd = CMD.ReqLogin,
+            // ReqLogin = new ReqLogin()
+            // {
+            //     Acct = "testAcc",
+            //     Psd = "testPsd",
+            // },
+            // Info = "test",
+            // Ping = new proto.test.Ping()
+            // {
+            //     IsOver = true,
+            // },
+            RspLogin = new RspLogin()
+            {
+                Info = new List<LoginInfo>()
+                {
+                    new LoginInfo()
+                    {
+                        Lv = "1",
+                        Exp = "10",
+                        Money = "100",
+                    }
+                }
+            }
+        };
+        
+        var bf = new ByteBuf();
+        msg.Serialize(bf);
+        Debug.Log(bf.Bytes.Length);
+        var bytes = bf.CopyData();
+        Debug.Log(bytes.Length);
+        var newBf = new ByteBuf(bytes);
+        Debug.Log(newBf.Bytes.Length);
+        var newMsg = new NetMsg();
+        newMsg.Deserialize(newBf);
+        Debug.Log(newMsg.ToString());
+    }
+
+    private JSONNode JsonLoader(string fileName)
+    {
+        return JSON.Parse(File.ReadAllText(Application.dataPath + "/../GenerateDatas/json/" + fileName + ".json"));
+    }
+    
+    void Update()
+    {
+        
+    }
+}
